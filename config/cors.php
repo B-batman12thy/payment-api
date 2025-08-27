62dbfebb76d3cb6d0cecf7896efa3b5c3968915c
B-batman12thy <thioubdjiby96@gmail.com>
2025-08-27 22:59:37 +0000

    add postgrel database

--- config/cors.php
+++ config/cors.php
@@ -4,13 +4,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:8080', 
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:8080',
-        'https://your-flutter-app.netlify.app',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
